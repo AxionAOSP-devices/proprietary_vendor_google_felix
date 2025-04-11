f9508ae8df358781c5cada59bf86a527165ae0f3
Michael Bestas <mkbestas@lineageos.org>
2025-04-11 21:22:42 +0300

    felix: Import blobs & firmware from BP1A.250405.007.B1
    Change-Id: I0b1407d98da9601cfb938bf29d86731e623c4276

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),felix)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,3e45eace97ca603abf162e0e8ab4f1fbb2de51a5)
-$(call add-radio-file-sha1-checked,radio/bl1.img,dbee284cc5a3dbcc28c36268448c1a893b1f5f58)
-$(call add-radio-file-sha1-checked,radio/bl2.img,358a21abbf060e260041f86fbb82db366fec566f)
-$(call add-radio-file-sha1-checked,radio/bl31.img,fc2c748b756c53d1cc1f474d0ce87cf4a72f9a25)
-$(call add-radio-file-sha1-checked,radio/gsa.img,83d9aeaf95024942e2f2f635e05f03b4289adb1e)
-$(call add-radio-file-sha1-checked,radio/ldfw.img,b06fae2e69561c0223fcbc892e6f616852ee5291)
+$(call add-radio-file-sha1-checked,radio/abl.img,dec4a7466d1a57c34859fc4a08c5f6add994d911)
+$(call add-radio-file-sha1-checked,radio/bl1.img,f578981bf514a75dd04052589a67d8e749e5c07b)
+$(call add-radio-file-sha1-checked,radio/bl2.img,fa9ac61ee4a124293d6dfa7de9199e3be7031c35)
+$(call add-radio-file-sha1-checked,radio/bl31.img,b97e434720f2cafe49a38f361147700a126438ca)
+$(call add-radio-file-sha1-checked,radio/gsa.img,e2c9a5aae1cebf91a1ca7f84ebd5860cdf28fa71)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,d02f372df385ce7638a8e0db6497ef5493494751)
 $(call add-radio-file-sha1-checked,radio/modem.img,05204be817636abf06a02bd4124b502afc0861db)
-$(call add-radio-file-sha1-checked,radio/pbl.img,02ce085b425f5e6f7adedec39563f77667bd73c0)
-$(call add-radio-file-sha1-checked,radio/tzsw.img,ae875e41d863894c68872a413741ade4402182da)
+$(call add-radio-file-sha1-checked,radio/pbl.img,06cb4de23d381b7401989ff5d5c17fd90b3a3977)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,c3e8fcd353fa8afd491d773a37a1e1c7e0240d43)
 
 endif
