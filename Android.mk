3f797dc0b44f35e581a093b42275cf4bf11c249b
Michael Bestas <mkbestas@lineageos.org>
2025-11-22 13:09:05 +0200

    felix: Update to BP3A.251105.015
    Change-Id: I9e13f9cc672dfac3f6ee26dded06defa6284e50d

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@ $(call add-radio-file-sha1-checked,radio/bl2.img,9e8d019102bf2c76669bbed940525b0
 $(call add-radio-file-sha1-checked,radio/bl31.img,5482c26dbb8f51bc95e23526680561c57727981e)
 $(call add-radio-file-sha1-checked,radio/gsa.img,88f9f144882e24ef093d2d7377b36de2c8197a69)
 $(call add-radio-file-sha1-checked,radio/ldfw.img,55ad35754b167871c7a35add104b66a80183f429)
-$(call add-radio-file-sha1-checked,radio/modem.img,4a4a23b0364478a6ced0e126ff707c6c86c3b1f0)
+$(call add-radio-file-sha1-checked,radio/modem.img,e97bebb652a42156caf888f66c6cbd8544b9bd84)
 $(call add-radio-file-sha1-checked,radio/pbl.img,4bf26901df47a1cf942bb2ac33e1f245cec499b0)
 $(call add-radio-file-sha1-checked,radio/tzsw.img,518a23c1151f4e2e77e9be619c610ed8b4f89bd5)
 
